eb8d8c21edbceaefc6d35113b3ebb904b4770a61
Chris DeLeon <gamkedo@gmail.com>
2015-11-16 00:24:21 -0800

    Scripts, textures, and prefabs added for Henry's 4 recent game designs (I'll code'em)
    Maze, Volcano, Raining Cats and Dogs, and Survival Swim. Also adjusted
    layout in main cabinet’s scene to have space for all machines

---
+++ Assets/Dynamic/RainingCatsAndDogs/GamePlayRCAD.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlayRCAD : PixelScreenLib {
+	float ballX = 25;
+	float ballY = 20;
+	float ballXV = 3.4f;
+	float ballYV = 1.4f;
+
+	public override void PerGameInput() {
+		if(Input.GetKey(KeyCode.LeftArrow) && ballXV > 0.0f) {
+			ballXV *= -1.0f;
+		}
+		if(Input.GetKey(KeyCode.RightArrow) && ballXV < 0.0f) {
+			ballXV *= -1.0f;
+		}
+		
+		if(Input.GetKey(KeyCode.UpArrow) && ballYV > 0.0f) {
+			ballYV *= -1.0f;
+		}
+		if(Input.GetKey(KeyCode.DownArrow) && ballYV < 0.0f) {
+			ballYV *= -1.0f;
+		}
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			ballX = Random.Range(0.0f, screenWidth);
+			ballY = Random.Range(0.0f, screenHeight);
+		}
+	}
+
+	private void ballBounceAndDraw() {
+		ballX += ballXV;
+		ballY += ballYV;
+		
+		if(ballX < 0 && ballXV < 0.0f) {
+			ballXV *= -1.0f;
+		}
+		if(ballX > screenWidth && ballXV > 0.0f) {
+			ballXV *= -1.0f;
+		}
+		if(ballY < 0 && ballYV < 0.0f) {
+			ballYV *= -1.0f;
+		}
+		if(ballY > screenHeight && ballYV > 0.0f) {
+			ballYV *= -1.0f;
+		}
+		
+		drawBoxAt((int)ballX-1,(int)ballY-1,3,greenCol);
+	}
+
+	void CenterBall() {
+		ballX = screenWidth/2;
+		ballY = screenHeight/2;
+	}
+
+	public override void PerGameStart() {
+		CenterBall();
+	}
+
+	public override void PerGameExit() {
+		CenterBall();
+	}
+
+	public override void PerGameDemoMode() {
+		ballBounceAndDraw(); // for this game just let the ball bounce
+
+		drawStringCentered(screenWidth/2,screenHeight/8,whiteCol,"Raining Cats");
+		drawStringCentered(screenWidth/2,screenHeight/8+8,redCol,"and Dogs");
+	}
+
+	public override void PerGameDemoModeCoinRequestDisplay() {
+		if( flashing ) {
+			drawStringCentered(screenWidth/2,screenHeight/2,greenCol,"INSERT TOKEN");
+		}
+	}
+
+	public override void PerGameTimerDisplay() {
+		drawStringCentered(screenWidth/2,screenHeight/2+10,yellowCol,
+		                   ""+ timerLeft);
+	}
+
+	public override void PerGameLogic() {
+		ballBounceAndDraw();
+	}
+	
+}
